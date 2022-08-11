7cedd74855a78a2df0012e8aff41717e80650de4
Amir Siraj <amaressabit0314@gmail.com>
2022-08-11 10:32:07 +0300

    show tasks from the database

--- lib/controller/task_controller.dart
+++ lib/controller/task_controller.dart
@@ -3,7 +3,20 @@ import 'package:new_cvsender/db/db_helper.dart';
 import 'package:new_cvsender/models/task.dart';
 
 class TaskController extends GetxController {
+  @override
+  void onRady() {
+    getTasks();
+    super.onReady();
+  }
+
+  var taskList = <Task>[].obs;
+
   Future<int> addTask({Task? task}) async {
     return await DBHelper.insert(task: task);
+  }
+
+  void getTasks() async {
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => Task.fromJson(data)).toList());
   }
 }
